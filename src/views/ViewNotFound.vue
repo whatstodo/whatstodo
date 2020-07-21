67f843377d73c003d5bb2aec934bff204758374f
arnoson <omasocke@hotmail.de>
2020-07-21 21:26:21 +0200

    Add 404 page
    Currently not handling invalid id for dynamic routes

---
+++ src/views/ViewNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="view view-not-found">
+    <Heading>{{ content.heading }}</Heading>
+  </div>
+</template>
+
+<script>
+import ViewMixin from '@/mixins/ViewMixin'
+import Heading from '@/components/Heading'
+
+export default {
+  mixins: [ViewMixin],
+
+  components: {
+    Heading
+  },
+
+  data() {
+    return {
+      contentId: 'not-found'
+    }
+  }
+}
+</script>
