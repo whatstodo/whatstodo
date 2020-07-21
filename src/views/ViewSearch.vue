4fb1745cbcf4c80ee7286963783b492586bab5c4
arnoson <omasocke@hotmail.de>
2020-07-21 21:31:18 +0200

    Show text version message on search

--- src/views/ViewSearch.vue
+++ src/views/ViewSearch.vue
@@ -1,3 +1,25 @@
 <template>
-  <div class="view view-search"></div>
+  <div class="view view-search">
+    <Heading :message="message">{{ content.heading }}</Heading>
+  </div>
 </template>
+
+<script>
+import ViewMixin from '@/mixins/ViewMixin'
+import Heading from '@/components/Heading'
+
+export default {
+  mixins: [ViewMixin],
+
+  components: {
+    Heading
+  },
+
+  data() {
+    return {
+      contentId: 'search',
+      messageId: 'test_version'
+    }
+  }
+}
+</script>
